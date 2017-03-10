bbc7dc77e203ee1d3ca9d164a474c30d032b0f56
samuel40791765 <samuelchungchiang@gmail.com>
2017-03-10 11:46:53 +0800

    rotating image added

--- Qt_Opengl_Framework/Qt_Opengl_Framework/Qt_Opengl_Framework/Application.cpp
+++ Qt_Opengl_Framework/Qt_Opengl_Framework/Qt_Opengl_Framework/Application.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e281e51c5eedd1a8d4d6269bc08ae1f6f85fc3d5b906f10879be3bf5cd5df4d3
-size 52762
+oid sha256:3f4ce19d87cb776c63b75bf142d5febba4d5f19b4a537f6b5e270be9154b593f
+size 53314
---
+++ Qt_Opengl_Framework/Qt_Opengl_Framework/media/materials/textures/ReadMe.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:01dd75568e0674bc85f19b7971784eacd7b57e6f7abdb3a42f6fa1fa182767b9
+size 29
